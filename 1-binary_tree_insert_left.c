40e2f235759f5742badf04058bfa501808f78a21
wafaa <wafaagaafar@gmail.com>
2024-02-27 06:31:19 -0800

    1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,21 +7,16 @@
  *
  * Return: Pointer to the created node, or NULL.
  */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
-
 	if (parent == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = binary_tree_node(parent, value);
+
 	if (new_node == NULL)
 		return (NULL);
-
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
 
 	if (parent->left != NULL)
 	{
