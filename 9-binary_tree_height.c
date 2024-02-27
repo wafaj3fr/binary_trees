37138ca459500aed39e441a8119173934cfc9b57
wafaa <wafaagaafar@gmail.com>
2024-02-27 14:22:43 -0800

    9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,23 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height_recursive - Measures the height.
- * @node: Pointer to node.
- *
- * Return: Height, or 0 if tree is NULL.
- */
-size_t binary_tree_height_recursive(const binary_tree_t *node)
-{
-	if (node == NULL)
-		return (0);
-
-	size_t left_height = binary_tree_height_recursive(node->left);
-	size_t right_height = binary_tree_height_recursive(node->right);
-
-	return ((left_height > right_height ? left_height : right_height) + 1);
-}
-
-/**
  * binary_tree_height - Measures the height of a tree.
  * @tree: Pointer to the root node.
  *
@@ -25,9 +8,20 @@ size_t binary_tree_height_recursive(const binary_tree_t *node)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height = 0;
+	size_t right_height = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_height_recursive(tree) - 1);
+	else
+	{
+		if (tree)
+		{
+			left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+			right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+		}
+		return (left_height > right_height ? left_height : right_height);
+	}
 }
 
